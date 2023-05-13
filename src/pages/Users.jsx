41ea52a5d3ab11df64cd2c406681907846832073
O. Riastanjung <oriastan999@gmail.com>
2023-05-13 21:59:54 +0700

    starting users page

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,16 @@
-
+import HeaderDashboard from "../parts/HeaderDashboard";
+import Sidebar from "../components/Sidebar";
 
 function Users() {
-    return (
-      <div>ini Users page</div>
-    )
-  }
-  
-  export default Users
+  return (
+    <section className="flex container mx-auto">
+      <Sidebar />
+      <main className="xl:w-[80%] w-[100%] h-[100vh] overflow-y-scroll   pt-6 px-10">
+        <HeaderDashboard />
+        
+      </main>
+    </section>
+  );
+}
+
+export default Users;
